daa27cc287db6632aff32109de69971dcbec4042
ryo kato <ryo@ryo-no-MacBook-Air.local>
2018-11-10 14:31:06 +0900

    add ngram parser

---
+++ parser.py
@@ -0,0 +1,23 @@
+
+
+class Parser:
+    def __init__(self, parser):
+        self.parser = parser
+
+    def parse(self, text):
+        return self.parser.parse(text)
+
+
+class NgramParser:
+
+    @classmethod
+    def parse(cls, n, text):
+        if len(text) < n:
+            return [text]
+        return [text[i: i + n] for i in range(0, len(text) - (n-1))]
+
+
+class BigramParser(NgramParser):
+    @classmethod
+    def parse(cls, text):
+        return super(BigramParser, cls).parse(2, text)
---
+++ test/test_parser.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+class TestParser:
+
+    def test_2gram_parser(self):
+        from parser import Parser, BigramParser
+
+        parser = Parser(BigramParser)
+        actual = parser.parse('test')
+        assert actual == ['te', 'es', 'st']
+
+
+class TestNgramParser:
+    @pytest.mark.parametrize(
+        "n,text,expected",
+        [
+            (1, 'test', ['t', 'e', 's', 't']),
+            (2, 'test', ['te', 'es', 'st']),
+            (3, 'test', ['tes', 'est']),
+            (4, 'test', ['test']),
+            (5, 'test', ['test'])
+        ]
+    )
+    def test_parse(self, n, text, expected):
+        from parser import NgramParser
+
+        actual = NgramParser.parse(n, text)
+        assert actual == expected
+
+
+class TestBigramParser:
+
+    def test_parse(self):
+        from parser import BigramParser
+
+        actual = BigramParser.parse('test')
+        assert actual == ['te', 'es', 'st']
